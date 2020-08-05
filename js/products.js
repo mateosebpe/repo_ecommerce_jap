c2888b3e11265d895ef4efe72b96b7650753a22f
Mateo Peña <mateosebpe@gmail.com>
2020-08-04 21:46:22 -0300

    Products
    Added generic product element in html and getJSONData of products in JS.

--- js/products.js
+++ js/products.js
@@ -1,6 +1,15 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+var currentProductsArray = [];
 
+function sortAndShowProducts (productsArray){
+currentProductsArray = productsArray;
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function (e) {
+getJSONData(PRODUCTS_URL).then(function(resultObj){
+    if(resultObj.status = "ok"){
+        sortAndShowProducts(resultObj.data);
+    }
+});
 });

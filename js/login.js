96fc53342de80babb5b2c1644bbd13328b5b7066
Mateo Peña <mateosebpe@gmail.com>
2020-08-07 10:37:54 -0300

    Google signin
    Added google login button

--- js/login.js
+++ js/login.js
@@ -4,3 +4,24 @@
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
+//Google SignIn
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    window.location.href = "cover.html"
+  }
+
+  function renderButton() {
+    gapi.signin2.render('my-signin2', {
+      'scope': 'profile email',
+      'width': 240,
+      'height': 50,
+      'longtitle': true,
+      'theme': 'dark',
+      'onsuccess': onSuccess,
+      'onfailure': onFailure
+    });
+  }

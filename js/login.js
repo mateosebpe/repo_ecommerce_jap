4164069b6a92f243f4c74a2cca294958b86b282c
mateosebpe <mateosebpe@gmail.com>
2020-08-26 10:59:54 -0300

    Fix google get name

--- js/login.js
+++ js/login.js
@@ -14,7 +14,7 @@ function onSignIn(googleUser) {
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    logged(profile.getName);
+    logged(profile.getName());
   }
   function renderButton() {
     gapi.signin2.render('my-signin2', {

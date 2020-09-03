7f43b994b254b23916b64f93eb7f786ea16c83b3
Mateo Peña <mateosebpe@gmail.com>
2020-09-02 21:11:09 -0300

    Comments url access
    Added json url access to show the comments

--- js/product-info.js
+++ js/product-info.js
@@ -1,45 +1,70 @@
 let imgPag = 0;
-let result;
 document.addEventListener("DOMContentLoaded", function (e) {
   getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
     if (resultObj.status == "ok") {
-      result = resultObj.data;
-      showInfo(result);
+      showInfo(resultObj.data);
+    }
+  });
+  getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj) {
+    if (resultObj.status == "ok") {
+      showComments(resultObj.data);
     }
   });
 });
 
-function showInfo(jsondata) {
+function showInfo(infoData) {
   let htmlContentToAppend = `<div class="row"> 
 <div class="col-md-6 text-center">
-  <img src="`+ jsondata.images[imgPag] + `" alt="" class="col">
+  <img src="`+ infoData.images[imgPag] + `" alt="" class="col">
   <button class="btn" id="previous">Anterior</button>
   <button class="btn" id="next">Siguiente</button>
 </div>
 <div class="col-md-6" >
-  <h2>`+ jsondata.name + `</h2>
-  <h4 class="text-success">`+ jsondata.cost + " " + jsondata.currency + `</h4>
-  <p class="text-info">`+ jsondata.soldCount + " vendidos.  |  " + "Categoría: " + jsondata.category + `</p>
-  <p style="overflow: auto;">`+ jsondata.description + `</p>
+  <h2>`+ infoData.name + `</h2>
+  <h4 class="text-success">`+ infoData.cost + " " + infoData.currency + `</h4>
+  <p class="text-info">`+ infoData.soldCount + " vendidos.  |  " + "Categoría: " + infoData.category + `</p>
+  <p style="overflow: auto;">`+ infoData.description + `</p>
 </div>`
 
   document.getElementById("product-info-display").innerHTML = htmlContentToAppend;
 
   document.getElementById("next").addEventListener("click", function () {
-    if (imgPag < jsondata.images.length - 1) {
+    if (imgPag < infoData.images.length - 1) {
       imgPag++;
     } else {
       imgPag = 0;
     }
-    showInfo(result);
+    showInfo(infoData);
   });
 
   document.getElementById("previous").addEventListener("click", function () {
     if (imgPag > 0) {
       imgPag--;
     } else {
-      imgPag = jsondata.images.length - 1;
+      imgPag = infoData.images.length - 1;
     }
-    showInfo(result);
+    showInfo(infoData);
   });
+}
+
+function showComments(comments){
+let htmlContentToAppend = "";
+
+for(let i = 0; i < comments.length; i++){
+  let starsHtml = "";
+  for(let h = 0; h < 5; h++){
+    if(h < comments[i].score){
+      starsHtml +=`<span class="fa fa-star checked"></span>`
+    } else{
+      starsHtml +=`<span class="fa fa-star"></span>`
+    }
+  }
+
+  htmlContentToAppend += `  <div class="list-group-item">
+  <p class="font-weight-bold" id="name">`+comments[i].user+`</p>
+  <p>`+comments[i].description+`</p>
+  <p class="text-info">`+comments[i].dateTime+ starsHtml+`</p>
+</div>`
+}
+document.getElementById("comments-display").innerHTML = htmlContentToAppend;
 }

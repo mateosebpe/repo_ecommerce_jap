5db098f555240bd7d0289b43925657cc34611ec8
mateosebpe <mateosebpe@gmail.com>
2020-10-06 09:49:20 -0300

    Cart visual
    Added html cart

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,52 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+currencySelector = document.getElementById('currencySelector');
+itemsArray = [];
 
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData('https://japdevdep.github.io/ecommerce-api/cart/654.json').then(resultObj => {
+        itemsArray = resultObj.data;
+        convertCurrency();
+    });
 });
+
+
+function makeElements() {
+    let htmlContent = ``;
+    itemsArray.articles.forEach(element => {
+        htmlContent +=
+            `<tr>
+            <td><img src="${element.src}" height="100px"></td>
+             <td>${element.name}</td>
+             <td><input type="number" value="${element.count}" onclick="calSubtotal();"></td>
+             <td>${element.unitCost + " " + element.currency}</td>
+             <td class="subtotal">${parseFloat(element.unitCost) * parseFloat(element.count) + " " + element.currency}</td>
+             </tr>`;
+    });
+    document.getElementById('elements-display').innerHTML = htmlContent;
+}
+
+function convertCurrency() {
+    itemsArray.articles.forEach(element => {
+        if (currencySelector.value == 0 && element.currency == "USD") {
+            element.unitCost = parseFloat(element.unitCost) * 40;
+            element.currency = "UYU"
+        }
+        if (currencySelector.value == 1 && element.currency == "UYU") {
+            element.unitCost = parseFloat(element.unitCost) / 40;
+            element.currency = "USD"
+        }
+    });
+    makeElements()
+}
+
+currencySelector.addEventListener('input', () => {
+    convertCurrency();
+});
+
+function calSubtotal(){
+    let subtotals = document.getElementsByClassName('subtotal');
+    let total = 0;
+    subtotals.forEach(subtotal =>{
+        total += subtotal.value;
+    });
+    console.log(total);
+}

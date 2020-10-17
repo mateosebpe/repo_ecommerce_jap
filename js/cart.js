474d2eb563cba5cc337c83f9cf6c44e5c59422f6
mateosebpe <mateosebpe@gmail.com>
2020-10-17 05:54:48 -0300

    Cart complete

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,16 @@
+const ENVIO_PREMIUM = 'premium';
+const ENVIO_EXPRESS = 'express';
+const ENVIO_STANDARD = 'standard';
+
 currencySelector = document.getElementById('currencySelector');
 itemsArray = [];
+//Variables para calcular el total
+let subtotal_principal;
+let metodo_de_envio = ENVIO_PREMIUM;
+let costo_de_envio;
+let moneda;
+
+
 
 document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData('https://japdevdep.github.io/ecommerce-api/cart/654.json').then(resultObj => {
@@ -8,13 +19,18 @@ document.addEventListener("DOMContentLoaded", function (e) {
     });
 });
 
+//----------------------------------------Seleccionar moneda--------------------------------------------------
+currencySelector.addEventListener('input', () => {
+    convertCurrency();
+});
 function convertCurrency() {
     itemsArray.articles.forEach(element => {
-        if (currencySelector.value == "UYU" && element.currency == "USD") {
+        moneda = currencySelector.value;
+        if (moneda == "UYU" && element.currency == "USD") {
             element.unitCost = parseFloat(element.unitCost) * 40;
             element.currency = "UYU"
         }
-        if (currencySelector.value == "USD" && element.currency == "UYU") {
+        if (moneda == "USD" && element.currency == "UYU") {
             element.unitCost = parseFloat(element.unitCost) / 40;
             element.currency = "USD"
         }
@@ -22,10 +38,10 @@ function convertCurrency() {
     });
     makeElements()
 }
-
+//----------------------------------------Crear elementos--------------------------------------------------
 function makeElements() {
     let htmlContent = ``;
-    itemsArray.articles.forEach(function(element,index) {
+    itemsArray.articles.forEach(function (element, index) {
         htmlContent +=
             `<tr>
             <td><img src="${element.src} " class="rounded" height="100px"></td>
@@ -33,34 +49,64 @@ function makeElements() {
              <td>${element.unitCost}</td>
              <td><input type="number" id="input${index}" value="${element.count}" min="0" onclick="changeSub(${element.unitCost},${index});"></td>
              <td id="subtotal${index}" class="text-left">${parseFloat(element.unitCost) * parseFloat(element.count)}</td>
-             <td>${element.currency}</td>
+             <td>${moneda}</td>
              </tr>`;
-             element.subtotal = parseFloat(element.unitCost) * parseFloat(element.count);
+        element.subtotal = parseFloat(element.unitCost) * parseFloat(element.count);
     });
     document.getElementById('elements-display').innerHTML = htmlContent;
     calcSub();
 }
-
+//----------------------------------------Cambiar cantidades------------------------------------------------
 function changeSub(cost, index) {
-    let subtotal = document.getElementById('subtotal' + index);
-    subtotal.innerText = document.getElementById('input' + index).value * cost;
+    let subtotal_secundario = document.getElementById('subtotal' + index);
+    subtotal_secundario.innerText = document.getElementById('input' + index).value * cost;
     itemsArray.articles[index].subtotal = document.getElementById('input' + index).value * cost;
     calcSub();
 }
-
+//----------------------------------------Seleccionar método de envío----------------------------------------
+document.getElementById('premium-tab').addEventListener('click', () => { 
+    metodo_de_envio = ENVIO_PREMIUM;
+    calcularEnvio();
+});
+document.getElementById('express-tab').addEventListener('click', () => { 
+    metodo_de_envio = ENVIO_EXPRESS;
+    calcularEnvio();
+});
+document.getElementById('standard-tab').addEventListener('click', () => { 
+    metodo_de_envio = ENVIO_STANDARD;
+    calcularEnvio();
+});
+//----------------------------------------Calcular subtotal--------------------------------------------------
 function calcSub() {
-    let total = 0;
-   // let subs = document.getElementByClassName('');
+    subtotal_principal = 0;
+    // let subs = document.getElementByClassName('');
     itemsArray.articles.forEach(element => {
-        total += element.subtotal;
+        subtotal_principal += element.subtotal;
     });
 
-document.getElementById('subtotal').innerHTML = total;
+    document.getElementById('subtotal').innerHTML = subtotal_principal;
+    document.getElementById('moneda').innerHTML = currencySelector.value;
 
-document.getElementById('moneda').innerHTML = currencySelector.value;
+    calcularEnvio();
 }
+//----------------------------------------Calcular total con envío---------------------------------------------
+function calcularEnvio() {
+    let htmlContent = `<td>${subtotal_principal + " " + moneda}</td>`;
+    switch (metodo_de_envio) {
+        case ENVIO_PREMIUM:
+            costo_de_envio = (subtotal_principal / 100) * 15;
+            htmlContent += `<td>${costo_de_envio + " " + moneda + " (15% del total)"}</td>`
+            break;
+        case ENVIO_EXPRESS:
+            costo_de_envio = (subtotal_principal / 100) * 7;
+            htmlContent += `<td>${costo_de_envio + " " + moneda + " (7% del total)"}</td>`
+            break;
+        case ENVIO_STANDARD:
+            costo_de_envio = (subtotal_principal / 100) * 5;
+            htmlContent += `<td>${costo_de_envio + " " + moneda + " (5% del total)"}</td>`
+            break;
+    }
+    htmlContent += `<th>${costo_de_envio + subtotal_principal + " " + moneda}</th>`
 
-
-currencySelector.addEventListener('input', () => {
-    convertCurrency();
-});
+    document.getElementById('tabla_total').innerHTML = htmlContent;
+}
